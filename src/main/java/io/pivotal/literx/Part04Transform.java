0dc44d0a0a25b02aa9025262dc9eedd9f5e629b6
Ernst Fastl <ernst@fastl.eu>
2021-11-24 12:11:50 +0100

    solved

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 /**
  * Learn how to transform values.
  *
@@ -11,29 +13,38 @@ import reactor.core.publisher.Mono;
  */
 public class Part04Transform {
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
-	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
-	}
+    // TODO Capitalize the user username, firstname and lastname
+    Mono<User> capitalizeOne(Mono<User> mono) {
+        Function<Mono<User>, Mono<User>> toUpper = userMono -> userMono.map(Part04Transform::toUpperCase);
+        return mono.transform(toUpper);
+    }
 
-//========================================================================================
+    private static User toUpperCase(User user) {
+        return new User(user.getUsername().toUpperCase(),
+                        user.getFirstname().toUpperCase(),
+                        user.getLastname().toUpperCase());
+    }
 
-	// TODO Capitalize the users username, firstName and lastName
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    //========================================================================================
 
-//========================================================================================
+    // TODO Capitalize the users username, firstName and lastName
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        Function<Flux<User>, Flux<User>> toUpper = uFlux -> uFlux.map(Part04Transform::toUpperCase);
+        return flux.transform(toUpper);
+    }
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
-	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    //========================================================================================
 
-	Mono<User> asyncCapitalizeUser(User u) {
-		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
-	}
+    // TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+    Flux<User> asyncCapitalizeMany(Flux<User> flux) {
+        return flux.flatMap(this::asyncCapitalizeUser);
+    }
 
+    Mono<User> asyncCapitalizeUser(User u) {
+        return Mono.just(new User(u.getUsername().toUpperCase(),
+                                  u.getFirstname().toUpperCase(),
+                                  u.getLastname().toUpperCase()));
+    }
 }
--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -5,6 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Learn how to use various other operators.
@@ -13,53 +14,56 @@ import java.util.List;
  */
 public class Part08OtherOperations {
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Create a Flux of user from Flux of username, firstname and lastname.
-	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
-	}
+    // TODO Create a Flux of user from Flux of username, firstname and lastname.
+    Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux,
+                                      Flux<String> firstnameFlux,
+                                      Flux<String> lastnameFlux) {
+        return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux)
+                   .map(tuple3 -> new User(tuple3.getT1(), tuple3.getT2(), tuple3.getT3()));
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Return the mono which returns its value faster
-	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
-	}
+    // TODO Return the mono which returns its value faster
+    Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
+        return Mono.first(mono1, mono2);
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Return the flux which returns the first value faster
-	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+    // TODO Return the flux which returns the first value faster
+    Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
+        return Flux.first(flux1, flux2);
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
-	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
-	}
+    // TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+    Mono<Void> fluxCompletion(Flux<User> flux) {
+        return flux.then();
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
-	Mono<User> nullAwareUserToMono(User user) {
-		return null;
-	}
+    // TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
+    Mono<User> nullAwareUserToMono(User user) {
+        return Mono.justOrEmpty(Optional.ofNullable(user));
+//        return Optional.ofNullable(user).map(Mono::just).orElseGet(Mono::empty);
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
-	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
-	}
+    // TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+    Mono<User> emptyToSkyler(Mono<User> mono) {
+        return mono.switchIfEmpty(Mono.just(User.SKYLER));
+    }
 
-//========================================================================================
+    //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
-	Mono<List<User>> fluxCollection(Flux<User> flux) {
-		return null;
-	}
-
+    // TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
+    Mono<List<User>> fluxCollection(Flux<User> flux) {
+        return flux.collectList();
+    }
 }
